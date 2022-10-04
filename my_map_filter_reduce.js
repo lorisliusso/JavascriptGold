251371ef4cf214d63bcaeeb26b27ae2338645cab
Loris Liusso <lorisliusso@gmail.com>
2022-10-04 09:57:00 +0200

    My map completed

--- my_map_filter_reduce.js
+++ my_map_filter_reduce.js
@@ -1,1 +1,11 @@
-const arr= [1,2,3,4,5]
+/*Without calling Array.prototype.map(), Array.prototype.filter(), Array.prototype.reduce(),
+we'll implement the following three similar functions on the Array prototype: */
+
+//MY MAP
+Array.prototype.myMap= function (callback){
+    const newArray= [];
+    for (i=0; i<this.length; i++){
+        newArray.push(callback(this[i]))
+    }
+    return newArray;
+}
