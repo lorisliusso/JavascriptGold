655d333fb943513a0c26e2fb280162c88455aff1
Loris Liusso <lorisliusso@gmail.com>
2022-10-04 10:01:52 +0200

    My filter completed

--- my_map_filter_reduce.js
+++ my_map_filter_reduce.js
@@ -2,10 +2,23 @@
 we'll implement the following three similar functions on the Array prototype: */
 
 //MY MAP
-Array.prototype.myMap= function (callback){
+Array.prototype.myMap= function(callback){
     const newArray= [];
     for (i=0; i<this.length; i++){
         newArray.push(callback(this[i]))
     }
     return newArray;
 }
+
+// MY FILTER
+Array.prototype.myFilter= function(callback){
+    const newArray= [];
+    for (i=0; i<this.length; i++){
+        if (callback(this[i]) === true){
+        newArray.push(this[i])
+     }
+    }
+    return newArray;
+}
+
+
